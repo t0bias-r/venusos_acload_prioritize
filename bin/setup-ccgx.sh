816afa9546a381b7ca397f4d3d3d61a76cfc67aa
Keshwarsingh Nadan <kn@xyberdata.com>
2023-02-05 11:21:41 +0400

    Update setup-ccgx.sh

--- bin/setup-ccgx.sh
+++ bin/setup-ccgx.sh
@@ -4,16 +4,16 @@ BASE=$(dirname $(dirname $(realpath "$0")))
 cd $BASE
 
 echo "Set up device service to autorun on restart"
-chmod +x $BASE/acload_prioritize__dbus_service.py
+chmod +x $BASE/acload_prioritize_dbus_service.py
 # Use awk to inject correct BASE path into the run script
 awk -v base=$BASE '{gsub(/\$\{BASE\}/,base);}1' $BASE/bin/service/run.tmpl >$BASE/bin/service/run
 chmod -R a+rwx $BASE/bin/service
-rm -f /service/acload_prioritize__dbus_service
-ln -s $BASE/bin/service /service/acload_prioritize__dbus_service
+rm -f /service/acload_prioritize_dbus_service
+ln -s $BASE/bin/service /service/acload_prioritize_dbus_service
 
-CMD="ln -s $BASE/bin/service /service/acload_prioritize__dbus_service"
+CMD="ln -s $BASE/bin/service /service/acload_prioritize_dbus_service"
 if ! grep -q "$CMD" /data/rc.local; then
     echo "$CMD" >> /data/rc.local
 fi
 chmod +x /data/rc.local
-echo "Setup acload_prioritize__dbus_service.py complete"
+echo "Setup acload_prioritize_dbus_service complete"
