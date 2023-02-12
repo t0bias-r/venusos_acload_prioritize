167b4e43f96f558ee738ab93aab193a173e4c500
Keshwarsingh Nadan <kn@xyberdata.com>
2023-02-13 00:00:05 +0400

    Update setup.sh

--- 2.92/bin/setup.sh
+++ 2.92/bin/setup.sh
@@ -10,7 +10,7 @@ chmod -R a+rwx $BASE/bin/service
 rm -f /service/dbus-acload-prioritize
 ln -s $BASE/bin/service /service/dbus-acload-prioritize
 
-CMD="ln -s $BASE/bin/service /service/dbus-acload-prioritize &"
+CMD="ln -s $BASE/bin/service /service/dbus-acload-prioritize"
 if ! grep -q "$CMD" /data/rc.local; then
     echo "$CMD" >> /data/rc.local
 fi
